521337776872196a6932c7a14bc92647cc1f5407
osemenets21 <oleg.min.vin@gmail.com>
2024-07-10 19:25:49 +0200

    create react app and newNote component

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { NewNote } from "./NewNote";
+
+export type Note = {
+  id: string;
+} & NoteData
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+}
+
+export type Tag = {
+  id: string;
+  label: string;
+}
+
+function App() {
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Hello</h1>} />
+        <Route path="/new" element={<NewNote/>} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
