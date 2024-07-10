7d18cbf1d5b6856ff5f4de4af4a4d58cfa1e441d
osemenets21 <oleg.min.vin@gmail.com>
2024-07-10 20:34:11 +0200

    add functionality for saving data in LocalStorage

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,26 +1,30 @@
-import { FormEvent, useRef } from "react";
+import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
-import { NoteData } from "./App";
+import { NoteData, Tag } from "./App";
+import { v4 as uuidV4 } from "uuid";
 
 type NoteFormProps = {
-    onSubmit: (data: NoteData) => void
-}
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
 
-export function NoteForm({onSubmit}: NoteFormProps) {
-    const titleRef = useRef<HTMLInputElement>(null);
-    const markdownRef = useRef<HTMLTextAreaElement>(null);
+export function NoteForm({ onSubmit, onAddTag, availableTags}: NoteFormProps) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const markdownRef = useRef<HTMLTextAreaElement>(null);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
 
-    function handleSubmit(e: FormEvent) {
-        e.preventDefault();
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
 
-        onSubmit({
-            title: titleRef.current!.value,
-            markdown: markdownRef.current!.value,
-            tags: [],
-        })
-    }
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: [],
+    });
+  }
 
   return (
     <Form onSubmit={handleSubmit}>
@@ -35,7 +39,27 @@ export function NoteForm({onSubmit}: NoteFormProps) {
           <Col>
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
-              <CreatableReactSelect isMulti />
+              <CreatableReactSelect
+                onCreateOption={(label) => {
+                  const newTag = { id: uuidV4(), label };
+                  onAddTag(newTag);
+                  setSelectedTags((prev) => [...prev, newTag]);
+                }}
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map(tag => {
+                    return { label: tag.label, value: tag.id}
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+                isMulti
+              />
             </Form.Group>
           </Col>
         </Row>
