3df2898a28845997c13e6b90048c1510f81d200a
osemenets21 <oleg.min.vin@gmail.com>
2024-07-11 19:27:59 +0200

    correct mistakes with LocalStorage (with tags)

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,32 +1,37 @@
 import { useMemo, useState } from "react";
-import { Button, Col, Row, Stack, Form, Card  } from "react-bootstrap";
-import { Link, useMatch } from "react-router-dom";
+import { Button, Col, Row, Stack, Form, Card, Badge } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import ReactSelect from "react-select";
 import { Tag } from "./App";
-import styles from "./NoteList.module.css"
-
+import styles from "./css/NoteList.css";
 
 type SimplifiedNote = {
   tags: Tag[];
   title: string;
   id: string;
-}
+};
 
 type NoteListProps = {
-    availableTags: Tag[];
-    notes: SimplifiedNote[];
-}
+  availableTags: Tag[];
+  notes: SimplifiedNote[];
+};
 
-export function NoteList({availableTags, notes}: NoteListProps) {
-    const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
-    const [title, setTitle] = useState("");
+export function NoteList({ availableTags, notes }: NoteListProps) {
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [title, setTitle] = useState("");
 
-    const filteredNotes = useMemo(() => {
-      return notes.filter(note => {
-        return (title === "" || note.title.toLowerCase().includes(title.toLowerCase())) && (selectedTags.length === 0 || selectedTags.every(tag => note.tags.some((noteTag: Tag) => noteTag.id === tag.id)))
-      })
-    }, [title, selectedTags, notes])
-
+  const filteredNotes = useMemo(() => {
+    return notes.filter((note) => {
+      return (
+        (title === "" ||
+          note.title.toLowerCase().includes(title.toLowerCase())) &&
+        (selectedTags.length === 0 ||
+          selectedTags.every((tag) =>
+            note.tags.some((noteTag: Tag) => noteTag.id === tag.id)
+          ))
+      );
+    });
+  }, [title, selectedTags, notes]);
 
   return (
     <>
@@ -35,31 +40,35 @@ export function NoteList({availableTags, notes}: NoteListProps) {
           <h1>Notes</h1>
         </Col>
         <Col xs="auto">
-            <Stack gap={2} direction="horizontal">
-                <Link to="/new">
-                    <Button variant="primary">Create</Button>
-                </Link>
-                <Button variant="outline-secondary">Edit Tags</Button>
-            </Stack>
+          <Stack gap={2} direction="horizontal">
+            <Link to="/new">
+              <Button variant="primary">Create</Button>
+            </Link>
+            <Button variant="outline-secondary">Edit Tags</Button>
+          </Stack>
         </Col>
       </Row>
       <Form>
         <Row className="mb-4">
-            <Col>
+          <Col>
             <Form.Group controlId="title">
-                <Form.Label>Title</Form.Label>
-                <Form.Control type="text" value={title} onChange={e => setTitle(e.target.value)}/>
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
             </Form.Group>
-            </Col>
-            <Col>
+          </Col>
+          <Col>
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
               <ReactSelect
                 value={selectedTags.map((tag) => {
                   return { label: tag.label, value: tag.id };
                 })}
-                options={availableTags.map(tag => {
-                    return { label: tag.label, value: tag.id}
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
                 })}
                 onChange={(tags) => {
                   setSelectedTags(
@@ -71,24 +80,48 @@ export function NoteList({availableTags, notes}: NoteListProps) {
                 isMulti
               />
             </Form.Group>
-            </Col>
+          </Col>
         </Row>
       </Form>
       <Row xs={1} sm={2} lg={3} xl={4} className="g-3">
-                {filteredNotes.map(note => (
-                  <Col key={note.id}>
-                    <NoteCard id={note.id} title={note.title} tags={note.tags}/>
-                  </Col>
-                ))}
+        {filteredNotes.map((note) => (
+          <Col key={note.id}>
+            <NoteCard id={note.id} title={note.title} tags={note.tags} />
+          </Col>
+        ))}
       </Row>
     </>
   );
 }
 
-function NoteCard({ id, title, tags}: SimplifiedNote) {
-  return <Card as={Link} to={`/${id}`} className={`h-100 text-reset text-decoration-none ${styles.card}`}>
-    <Card.Body>
-      
-    </Card.Body>
-  </Card>
+function NoteCard({ id, title, tags }: SimplifiedNote) {
+  return (
+    <Card
+      as={Link}
+      to={`/${id}`}
+      className={`h-100 text-reset text-decoration-none ${styles.card}`}
+    >
+      <Card.Body>
+        <Stack
+          gap={2}
+          className="align-items-center justify-content-center h-100"
+        >
+          <span className="fs-5">{title}</span>
+          {tags.length > 0 && (
+            <Stack
+              gap={1}
+              direction="horizontal"
+              className="justify-content-center flex-wrap"
+            >
+              {tags.map(tag => (
+                <Badge className="text-truncate" key={tag.id}>
+                  {tag.label}
+                </Badge>
+              ))}
+            </Stack>
+          )}
+        </Stack>
+      </Card.Body>
+    </Card>
+  )
 }
